32852f7b9df5587f850dcb7779ed04f8614867d9
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-04-13 01:34:55 +0530

    finalised

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Home from "./Components/Home/Home";
+import LandingPage from "./Components/LandingPage/LandingPage";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home2 from "../Home2/Home2";
+import Navbar from "../../Components/Navbar/Navbar";
+const LandingPage = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Home2 />
+    </div>
+  );
+};
+
+export default LandingPage;
